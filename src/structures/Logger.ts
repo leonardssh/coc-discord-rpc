7f22a29cc257f9756bb78d5a067fc420c92c5b6c
leonardssh22@gmail.com <leonardssh22@gmail.com>
2020-11-06 20:08:45 +0100

    chore: cleanup build branch

--- src/structures/Logger.ts
+++
@@ -1,9 +0,0 @@
-import { workspace } from 'coc.nvim';
-
-export enum LogLevel {
-	Info = 'more',
-	Warn = 'warning',
-	Err = 'error'
-}
-
-export const log = (logMsg: string, logLevel: LogLevel) => workspace.showMessage(`RPC: ${logMsg}`, logLevel);

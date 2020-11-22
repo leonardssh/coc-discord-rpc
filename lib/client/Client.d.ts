0d082632d0471a8b64941eea0e7c9617830deb7b
LeonardSSH <LeonardSSH@users.noreply.github.com>
2020-11-22 18:54:03 +0000

    build: output for 09e72d0af0ff7e8cbc4e9b13f08e47d0ab7bc289

--- lib/client/Client.d.ts
+++ lib/client/Client.d.ts
@@ -10,5 +10,6 @@ export default class Client implements Disposable {
     dispose(): Promise<void>;
     disconnect(): Promise<void>;
     private registerCommands;
+    private isWorkspaceIgnored;
 }
 //# sourceMappingURL=Client.d.ts.map

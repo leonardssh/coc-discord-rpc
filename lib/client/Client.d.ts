13fb966bdb28e633a64c6dc48229d399b605b9b3
LeonardSSH <LeonardSSH@users.noreply.github.com>
2020-11-29 13:16:19 +0000

    build: output for 5006d5668ad38c0ee83c403caeeda37eac9a5fd8

--- lib/client/Client.d.ts
+++ lib/client/Client.d.ts
@@ -6,7 +6,7 @@ export default class Client implements Disposable {
     constructor(config: WorkspaceConfiguration);
     connect(ctx?: ExtensionContext, _log?: boolean): Promise<void>;
     ready(ctx?: ExtensionContext, _log?: boolean): void;
-    setActivity(): Promise<void>;
+    setActivity(workspaceElapsedTime?: boolean): Promise<void>;
     dispose(): Promise<void>;
     disconnect(): Promise<void>;
     private registerCommands;

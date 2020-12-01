d4a9820943cdaf6494bd568de8d5ac9cac6b90d5
LeonardSSH <LeonardSSH@users.noreply.github.com>
2020-12-01 22:18:06 +0000

    build: output for fcc78966e825e3de8c56ba0c376cb70f31a29fed

--- lib/client/Client.d.ts
+++ lib/client/Client.d.ts
@@ -1,14 +1,13 @@
-import { Disposable, ExtensionContext, WorkspaceConfiguration } from 'coc.nvim';
+import { Disposable, WorkspaceConfiguration } from 'coc.nvim';
 export default class Client implements Disposable {
     config: WorkspaceConfiguration;
     private rpc?;
     private readonly activity;
     constructor(config: WorkspaceConfiguration);
-    connect(ctx?: ExtensionContext, _log?: boolean): Promise<void>;
-    ready(ctx?: ExtensionContext, _log?: boolean): void;
+    connect(): Promise<void>;
+    ready(): void;
     setActivity(workspaceElapsedTime?: boolean): Promise<void>;
     dispose(): Promise<void>;
     disconnect(): Promise<void>;
-    private registerCommands;
 }
 //# sourceMappingURL=Client.d.ts.map

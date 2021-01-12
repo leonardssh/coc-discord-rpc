9281c13eee97111c3304bd2f8dbd770e92a071eb
LeonardSSH <leonardssh22@gmail.com>
2021-01-12 20:19:21 +0100

    build: add webpack

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-
 import type { ExtensionContext } from 'coc.nvim';
-// @ts-ignore
-import { name as EXTENSION_NAME, version as EXTENSION_VERSION } from '../package.json';
 import { LoggingService } from './services/logging';
 
-const extensionName = EXTENSION_NAME || 'dev.coc-discord-rpc';
-const extensionVersion = EXTENSION_VERSION || '0.0.0';
+const extensionName = process.env.EXTENSION_NAME || 'dev.coc-discord-rpc';
+const extensionVersion = process.env.EXTENSION_VERSION || '0.0.0';
 
 const loggingService = new LoggingService();
 
 export const activate = (ctx: ExtensionContext) => {
-	loggingService.logInfo(`=== Extension activate ===`, ctx);
+	loggingService.logInfo(`=== Extension activated ===`, ctx);
 	loggingService.logInfo(`Extension Name: ${extensionName}.`, ctx);
 	loggingService.logInfo(`Extension Version: ${extensionVersion}.`, ctx);
 };
 
 export const deactivate = () => {
-	loggingService.logInfo(`=== Extension deactivate ===`);
+	loggingService.logInfo(`=== Extension deactivated ===`);
 };

ace8faf8e3fca330a61ea87b8f5ea0c6511c70cf
Narcis B <leonardssh22@gmail.com>
2021-06-08 16:34:17 +0200

    chore: cleanup build branch

---
+++ build.sh
@@ -0,0 +1,7 @@
+rm -rf "F:\\VirtualBox VMs\\Pop__OS_shared\\coc-discord-rpc"
+
+mkdir -p "F:\\VirtualBox VMs\\Pop__OS_shared\\coc-discord-rpc\\dist"
+
+cp -i -r dist "F:\\VirtualBox VMs\\Pop__OS_shared\\coc-discord-rpc"
+
+cp -i -r package.json "F:\\VirtualBox VMs\\Pop__OS_shared\\coc-discord-rpc"

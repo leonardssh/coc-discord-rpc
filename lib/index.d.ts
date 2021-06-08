0f4d0a3a7c03e620c36ce44e0d9197c7293beadc
Narcis B <leonardssh22@gmail.com>
2021-06-08 16:35:32 +0200

    chore: cleanup build branch

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ExtensionContext } from 'coc.nvim';
-export declare const activate: (ctx: ExtensionContext) => Promise<void>;
-export declare const deactivate: () => Promise<void>;
-//# sourceMappingURL=index.d.ts.map
